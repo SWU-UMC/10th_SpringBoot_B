9f2598f7194ca740faff32e390ebec2a2b40be59
yujining3827 <j_yujin_swu22@naver.com>
2026-04-29 15:04:32 +0900

    fix : repository를 interface로 변경하고 JpaRepository 적용

--- src/main/java/com/example/umc10th/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/umc10th/domain/member/repository/MemberRepository.java
@@ -1,7 +1,7 @@
 package com.example.umc10th.domain.member.repository;
 
-import org.apache.catalina.User;
+import com.example.umc10th.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<User, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
