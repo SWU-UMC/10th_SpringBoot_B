9ff1ee752ea8115e18db8be948e05065a731faf8
yujining3827 <j_yujin_swu22@naver.com>
2026-04-29 15:05:18 +0900

    chore : 폴더명 오타 수정

--- src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
@@ -1,6 +1,6 @@
 package com.example.umc10th.domain.mission.repository;
 
-import com.example.umc10th.domain.mission.erntity.Mission;
+import com.example.umc10th.domain.mission.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
