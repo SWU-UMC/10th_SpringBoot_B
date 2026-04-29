0b44d0bf2e00c52248033130f4966edfbada9a80
yujining3827 <j_yujin_swu22@naver.com>
2026-04-30 00:13:17 +0900

    feat : 회원가입 api 구조 구현

--- src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -1,4 +1,25 @@
 package com.example.umc10th.domain.member.controller;
 
+import com.example.umc10th.domain.member.dto.MemberReqDTO;
+import com.example.umc10th.domain.member.dto.MemberResDTO;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class MemberController {
+
+    @PostMapping("/signup")
+    public ApiResponse<MemberResDTO.SignupDTO> signup(
+            @RequestBody MemberReqDTO.SignupDTO request
+    ) {
+
+        MemberResDTO.SignupDTO response = new MemberResDTO.SignupDTO();
+        response.userId = 1L;
+        response.message = "회원가입이 완료되었습니다.";
+
+        return ApiResponse.onSuccess(response);
+    }
 }
--- src/main/java/com/example/umc10th/domain/member/dto/MemberResDTO.java
+++ src/main/java/com/example/umc10th/domain/member/dto/MemberResDTO.java
@@ -1,4 +1,10 @@
 package com.example.umc10th.domain.member.dto;
 
 public class MemberResDTO {
+
+    public static class SignupDTO {
+        public Long userId;
+        public String message;
+    }
+
 }
