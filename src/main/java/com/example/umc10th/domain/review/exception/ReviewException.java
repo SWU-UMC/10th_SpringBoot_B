b637cedf87f4f9d7cb372f178e745232b68b8c7e
yujining3827 <j_yujin_swu22@naver.com>
2026-04-30 14:37:52 +0900

    refactor: 기존 루트 src 제거 및 luke 구조로 재구성

--- src/main/java/com/example/umc10th/domain/review/exception/ReviewException.java
+++
@@ -1,4 +0,0 @@
-package com.example.umc10th.domain.review.exception;
-
-public class ReviewException {
-}
--- src/main/java/com/example/umc10th/domain/review/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.umc10th.domain.review.repository;
-
-import com.example.umc10th.domain.review.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
