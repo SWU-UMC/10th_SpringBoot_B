f996beecc3189193931ac0e6f1fe60323d304abc
yujining3827 <j_yujin_swu22@naver.com>
2026-05-04 15:45:51 +0900

    Delete: luke/src directory

--- luke/src/main/java/com/example/umc10th/global/apiPayload/code/BaseErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.example.umc10th.global.apiPayload.code;
-
-import org.springframework.http.HttpStatus;
-
-public interface BaseErrorCode {
-    HttpStatus getStatus();
-    String getCode();
-    String getMessage();
-}
