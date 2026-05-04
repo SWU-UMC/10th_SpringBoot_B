d50fc80c5be83e9efa36ea35ea320821b763d6c6
yujining3827 <j_yujin_swu22@naver.com>
2026-05-04 14:36:50 +0900

    fix : api 기본 경로 /api/v1으로 변경 및 리소스 중심으로 설계 수정

--- luke/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ luke/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
 
