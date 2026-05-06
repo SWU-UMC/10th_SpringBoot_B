fa3f2a96c077d2c84602c0ce6d64dc8ffd7d45f1
ywkim1m <ywkim1m@gmail.com>
2026-05-06 22:25:53 +0900

    feat: ReviewSuccessCode 추가

--- umc10th_wony/src/main/java/com/example/umc10th_wony/domain/review/exception/code/ReviewSuccessCode.java
+++ umc10th_wony/src/main/java/com/example/umc10th_wony/domain/review/exception/code/ReviewSuccessCode.java
@@ -1,4 +1,17 @@
 package com.example.umc10th_wony.domain.review.exception.code;
 
-public enum ReviewSuccessCode {
+import com.example.umc10th_wony.global.apiPayload.code.BaseSuccessCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ReviewSuccessCode implements BaseSuccessCode {
+
+    REVIEW_CREATED(HttpStatus.CREATED, "REVIEW200_1", "리뷰 생성에 성공했습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
--- umc10th_wony/src/main/java/com/example/umc10th_wony/domain/review/service/ReviewService.java
+++ umc10th_wony/src/main/java/com/example/umc10th_wony/domain/review/service/ReviewService.java
@@ -1,4 +1,42 @@
 package com.example.umc10th_wony.domain.review.service;
 
+import com.example.umc10th_wony.domain.member.entity.Member;
+import com.example.umc10th_wony.domain.member.exception.MemberException;
+import com.example.umc10th_wony.domain.member.exception.code.MemberErrorCode;
+import com.example.umc10th_wony.domain.member.repository.MemberRepository;
+import com.example.umc10th_wony.domain.mission.entity.Mission;
+import com.example.umc10th_wony.domain.mission.exception.MissionException;
+import com.example.umc10th_wony.domain.mission.exception.code.MissionErrorCode;
+import com.example.umc10th_wony.domain.mission.repository.MissionRepository;
+import com.example.umc10th_wony.domain.review.converter.ReviewConverter;
+import com.example.umc10th_wony.domain.review.dto.ReviewReqDTO;
+import com.example.umc10th_wony.domain.review.dto.ReviewResDTO;
+import com.example.umc10th_wony.domain.review.entity.Review;
+import com.example.umc10th_wony.domain.review.repository.ReviewRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final MissionRepository missionRepository;
+
+    @Transactional
+    public ReviewResDTO createReview(ReviewReqDTO request) {
+
+        Member member = memberRepository.findById(request.getMemberId())
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        Mission mission = missionRepository.findById(request.getMissionId())
+                .orElseThrow(() -> new MissionException(MissionErrorCode.MISSION_NOT_FOUND));
+
+        Review review = ReviewConverter.toEntity(request, member, mission);
+
+        reviewRepository.save(review);
+
+        return ReviewConverter.toResponse(review);
+    }
 }
