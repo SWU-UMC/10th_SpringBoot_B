01b33f0b53219ec38208f478a1e79f5a15e6f85a
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-05-07 08:02:58 +0900

    feat: 6주차 미션_Mission domain Entity 생성

--- luga/src/main/java/com/example/umc10th/domain/mission/entity/mapping/UserMission.java
+++ luga/src/main/java/com/example/umc10th/domain/mission/entity/mapping/UserMission.java
@@ -1,4 +1,43 @@
 package com.example.umc10th.domain.mission.entity.mapping;
 
+import com.example.umc10th.domain.mission.entity.Mission;
+import com.example.umc10th.domain.mission.enums.MissionStatus;
+import com.example.umc10th.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_mission")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class UserMission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private MissionStatus status = MissionStatus.IN_PROGRESS;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime completedAt;
+
+    public void complete() {
+        this.status = MissionStatus.COMPLETE;
+        this.completedAt = LocalDateTime.now();
+    }
 }
