b3684b3f35a929661452a9dc6ef266a115abe87c
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-05-07 07:53:23 +0900

    feat: 6주차 미션_User domain Entity 생성

--- luga/src/main/java/com/example/umc10th/domain/user/entity/User.java
+++ luga/src/main/java/com/example/umc10th/domain/user/entity/User.java
@@ -1,8 +1,12 @@
 package com.example.umc10th.domain.user.entity;
 
 import com.example.umc10th.domain.user.enums.Gender;
+import com.example.umc10th.domain.user.enums.SocialLogin;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -21,5 +25,28 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    private LocalDate birth;
+    private String address;
+    private String addressDetail;
+    private String email;
+    private String phone;
 
+    @Builder.Default
+    private Integer phoneChecked = 0;
+
+    @Enumerated(EnumType.STRING)
+    private SocialLogin socialLogin;
+
+    private String userPoint;
+
+    @Builder.Default
+    private Integer newAlarm = 0;
+
+    // 약관동의 도메인 생성 후 작업
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "agree_id")
+//    private Agreement agreement;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
