d253bd1dc2154ee3eaff95139dca939ef53dd899
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-05-07 07:40:11 +0900

    fix: 5주차 미션_build.gradle 내 버전 수정

--- luga/src/main/java/com/example/umc10th/domain/user/entity/User.java
+++ luga/src/main/java/com/example/umc10th/domain/user/entity/User.java
@@ -1,4 +1,25 @@
 package com.example.umc10th.domain.user.entity;
 
+import com.example.umc10th.domain.user.enums.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
 }
