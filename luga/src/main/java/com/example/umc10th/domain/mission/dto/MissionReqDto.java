56dde26dd218f8da1abe6c23605e6b5ce9ec1693
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-05-07 05:57:04 +0900

    fix: 5주차 미션_MissionController & MissionReqDto 수정 : 스웨거 명세 추가, missionId를 경로 변수로 받도록 수정

--- luga/src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDto.java
+++ luga/src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDto.java
@@ -1,11 +1,4 @@
 package com.example.umc10th.domain.mission.dto;
 
-import lombok.Getter;
-
 public class MissionReqDto {
-
-    @Getter
-    public static class MissionSuccessReqDto {
-        private Long missionId;
-    }
 }
