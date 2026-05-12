57bdc45195939c1407df60ef0eef2d891ec1a8f5
yujining3827 <j_yujin_swu22@naver.com>
2026-05-12 16:38:51 +0900

    refactor: ReviewService readOnly 트랜잭션 적용

--- luke/umc10th/src/main/java/com/example/umc10th/domain/review/service/ReviewService.java
+++ luke/umc10th/src/main/java/com/example/umc10th/domain/review/service/ReviewService.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
