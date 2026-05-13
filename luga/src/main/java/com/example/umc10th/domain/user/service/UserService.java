5e424651c37877034670568b22febcccd9874489
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-05-14 02:03:08 +0900

    fix: 6주차 미션_user converter 추가를 통한 로직 분리 및 Slice 페이징 적용

--- luga/src/main/java/com/example/umc10th/domain/user/service/UserService.java
+++ luga/src/main/java/com/example/umc10th/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.umc10th.domain.user.service;
 
+import com.example.umc10th.domain.user.converter.UserConverter;
 import com.example.umc10th.domain.user.dto.UserReqDto;
 import com.example.umc10th.domain.user.dto.UserResDto;
 import com.example.umc10th.domain.user.entity.User;
@@ -16,16 +17,10 @@ public class UserService {
 
     @Transactional
     public UserResDto.SignupResDto signup(UserReqDto.SignupReqDto request) {
-        User user = User.builder()
-                .name(request.getName())
-                .email(request.getEmail())
-                .phone(request.getPhone())
-                .address(request.getAddress())
-                .build();
+        User user = UserConverter.toUser(request);
+
         User saved = userRepository.save(user);
-        return UserResDto.SignupResDto.builder()
-                .userId(saved.getId())
-                .createdAt(saved.getCreatedAt())
-                .build();
+
+        return UserConverter.toSignupResDto(saved);
     }
 }
