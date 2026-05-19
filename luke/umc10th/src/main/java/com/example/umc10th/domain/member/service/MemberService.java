fe659064844d103ee2c1a31202558d742db47eed
yujining3827 <j_yujin_swu22@naver.com>
2026-05-19 17:55:53 +0900

    feat: MemberService에 의존성 추가

--- luke/umc10th/src/main/java/com/example/umc10th/domain/member/service/MemberService.java
+++ luke/umc10th/src/main/java/com/example/umc10th/domain/member/service/MemberService.java
@@ -4,9 +4,14 @@ import com.example.umc10th.domain.member.converter.MemberConverter;
 import com.example.umc10th.domain.member.dto.MemberResDTO;
 import com.example.umc10th.domain.member.entity.Member;
 import com.example.umc10th.domain.member.repository.MemberRepository;
+import com.example.umc10th.domain.mission.repository.MarketRepository;
+import com.example.umc10th.domain.mission.repository.MissionRepository;
+import com.example.umc10th.domain.mission.repository.ParticipateRepository;
 import com.example.umc10th.domain.review.exception.ReviewException;
 import com.example.umc10th.domain.review.exception.code.ReviewErrorCode;
+import com.example.umc10th.domain.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +21,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final MarketRepository marketRepository;
+    private final MissionRepository missionRepository;
+    private final ParticipateRepository participateRepository;
+    private final ReviewRepository reviewRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public MemberResDTO.MyPageDTO getMyPage(Long memberId) {
 
