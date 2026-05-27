1502139e6aab664eb2be3eef5a36cf83d8740712
yujining3827 <j_yujin_swu22@naver.com>
2026-05-27 13:55:59 +0900

    refactor: SecurityConfig 의존성 주입 방식 정리

--- luke/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
+++ luke/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
     };
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomAuthenticationEntryPoint customAuthenticationEntryPoint, CustomAccessDeniedHandler customAccessDeniedHandler) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
 
